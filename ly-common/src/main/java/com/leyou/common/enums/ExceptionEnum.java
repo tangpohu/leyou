b122f71d91b2379aa848d5b9a918d467be8dff45
唐伯虎 <2275262060@qq.com>
2019-03-06 20:53:08 +0800

    提交03-06

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -7,12 +7,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public enum ExceptionEnum {
+public enum  ExceptionEnum {
     PRICE_CANNOT_BE_NULL( 400 , "价格不能空！"),
     CATEGORY_NOT_FOND(404,"商品分类没查到"),
     BRAND_NOT_FOUND(404,"品牌不存在"),
     BRAND_SAVE_ERROR(500,"新增品牌失败"),
     CATEGORY_BRAND_SAVE_ERROR(500,"新增品牌分类中间表失败"),
+    UPLOAD_ERROR(500,"文件上传失败"),
+    INVALID_FILE_TYPE(500,"上传失败，文件类型不匹配"),
+
 
     ;
     private int code;
